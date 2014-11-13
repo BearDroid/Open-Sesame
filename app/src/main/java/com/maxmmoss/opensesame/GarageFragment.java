99c2fecb812dfc0748214f196aa05ede85ccc64f
BearDroid <mossmm770@gmail.com>
2014-11-12 23:52:54 -0600

    Added Lollipop Toolbar, navigation drawer, fragment support, main buttons and layout

---
+++ app/src/main/java/com/maxmmoss/opensesame/GarageFragment.java
@@ -0,0 +1,19 @@
+package com.maxmmoss.opensesame;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Max on 11/12/2014.
+ */
+public class GarageFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/maxmmoss/opensesame/NavigationDrawerCallbacks.java
@@ -0,0 +1,8 @@
+package com.maxmmoss.opensesame;
+
+/**
+ * Created by Max on 11/12/2014.
+ */
+public interface NavigationDrawerCallbacks {
+    void onNavigationDrawerItemSelected(int position);
+}
